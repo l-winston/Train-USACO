36de6e837271ce3666e76daef992c1fcff242143
Winston Liu <winwinstonliu@gmail.com>
2018-09-11 17:23:32 -0700

    square solution added

---
+++ 2016.12.Bronze/src/square.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class square {
+
+    public static void main(String[] args) throws IOException {
+        final String probName = "square";
+        Scanner in = new Scanner(new File(probName + ".in"));
+        PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(probName + ".out")));
+
+        Point p1 = new Point(in.nextInt(), in.nextInt());
+        Point p2 = new Point(in.nextInt(), in.nextInt());
+        Point p3 = new Point(in.nextInt(), in.nextInt());
+        Point p4 = new Point(in.nextInt(), in.nextInt());
+
+        int x1 = Math.min(p1.x, p3.x);
+        int x2 = Math.max(p2.x, p4.x);
+        int y1 = Math.min(p1.y, p3.y);
+        int y2 = Math.max(p2.y, p4.y);
+
+        int maxside = Math.max(x2-x1, y2-y1);
+
+        out.println(maxside*maxside);
+
+        in.close();
+        out.close();
+        System.exit(0);
+    }
+}
+
+class Point{
+    int x, y;
+    public Point(int _x, int _y){
+        x=_x;
+        y=_y;
+    }
+}
